5b1b4c889f2c4e6abee3604782a5e82b7a06d866
Rubyroy12 <you@example.com>
2021-07-02 02:26:23 +0300

    add images to display in templates

--- gallery/images/views.py
+++ gallery/images/views.py
@@ -3,5 +3,7 @@ from .models import Images,Category,Location
 
 # Create your views here.
 def home(request):
-    images = Images.get_image()
-    return render(request,'index.html',{'images':images})
+    gallery = Images.objects.all()
+    return render(request,'index.html',{'gallery':gallery})
+
+    
