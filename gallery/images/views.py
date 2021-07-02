12e668e3c3a35685d7de75038cfccbdd369d01ba
Rubyroy12 <you@example.com>
2021-07-02 13:22:23 +0300

    style homepage

--- gallery/images/views.py
+++ gallery/images/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from .models import Images,Category,Location
+from django.http import Http404
 
 # Create your views here.
 def home(request):
@@ -11,4 +12,14 @@ def gallery(request):
     gallery = Images.objects.all()
     return render(request,'gallery.html',{'gallery':gallery})
 
+def single_image(request,image_id):
+    try:
+        image = Images.objects.get(id=image_id)
+    except DoesNotExist:
+        Http404()
     
+    return render(request,'single_image.html',{'image':image})
+    
+    
+
+
